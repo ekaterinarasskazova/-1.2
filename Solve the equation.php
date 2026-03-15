8619e7c31925b0307b7e14345c516a4913537fc3
FrontendCourseMP <rasskazovaekaterina20@gmail.com>
2026-03-16 02:52:24 +0300

    определение какая операция используется

--- Solve the equation.php
+++ Solve the equation.php
@@ -1,4 +1,13 @@
 <?php
 $equation = "4*X=36";
 $equation = str_replace(" ", "", $equation);
-list($left, $right) = explode("=", $equation); 
+list($left, $right) = explode("=", $equation);
+if (strpos($left, "+") !== false) {
+    $operator = "+";
+} elseif (strpos($left, "-") !== false) {
+    $operator = "-";
+} elseif (strpos($left, "*") !== false) {
+    $operator = "*";
+} elseif (strpos($left, "/") !== false) {
+    $operator = "/";
+} 
